03912fa131af555fd28d14cf550425a54ab61433
Maksym Yarmola <152811314+maksdhdh@users.noreply.github.com>
2024-03-12 20:33:18 +0200

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        var numbers = Enumerable.Range(-10, 11);
+
+        bool foundPositive = false;
+        foreach (var number in numbers)
+        {
+            if (number > 0)
+            {
+                Console.WriteLine(number);
+                foundPositive = true;
+                break;
+            }
+        }
+
+        if (!foundPositive)
+        {
+            Console.WriteLine(0);
+        }
+    }
+}
